ea0189997dc75ab46d786fcf9a6e3e62e5868b98
kavin-arasu-0206 <skavinarasu7@gmail.com>
2026-02-23 12:47:41 +0530

    Add files via upload

---
+++ OPPS_banneer.java
@@ -0,0 +1,15 @@
+/**
+* 00PSBannerApp UC1 00PS Banner Display Application
+*
+* This class demonstrates a simple Java application that displays the Object
+* Oriented Programming System OOPS acronym to the console.
+*
+* @author Developer
+* @version 1.0
+/*
+public class 00PSBannerApp {
+Run | Debug
+public static void main(String[] args) {
+System.out.println(x: "00PS");
+}
+}
